59784d806aedadfa75c521b65a728a0752f5692e
André Gonçalves <andre.goncalves@outsystems.com>
2019-07-12 10:33:24 +0100

    updated the install_prerequisites

--- hooks/install_prerequisites.js
+++ hooks/install_prerequisites.js
@@ -1,8 +1,8 @@
 console.log("Running hook to install image editor plugin pre-requisites");
 
 module.exports = function (context) {
-  var child_process = context.requireCordovaModule('child_process'),
-      deferral = context.requireCordovaModule('q').defer();
+  var child_process = require('child_process'),
+      deferral = require('q').defer();
 
   var output = child_process.exec('npm install', {cwd: __dirname}, function (error) {
     if (error !== null) {
