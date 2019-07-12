495ee45cd7cb930b63162e21909b2134162cd9bc
André Gonçalves <andre.goncalves@outsystems.com>
2019-07-12 11:15:35 +0100

    added q and child process and removed xcode. the idea is to include the libraries needed to install the rest of the dependencies

---
+++ hooks/install_prerequisites.js
@@ -0,0 +1,19 @@
+console.log("Running hook to install image editor plugin pre-requisites");
+
+module.exports = function (context) {
+  var child_process = require('child_process'),
+      deferral = require('q').defer();
+
+  var output = child_process.exec('npm install', {cwd: __dirname}, function (error) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      deferral.reject('npm installation failed');
+    }
+    else {
+      deferral.resolve();
+    }
+  });
+
+  return deferral.promise;
+};
+ No newline at end of file
