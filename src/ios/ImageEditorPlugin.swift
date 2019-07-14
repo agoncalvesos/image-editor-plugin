4c6678974e4ecb75797f5764366adc4df6b85e1c
André Gonçalves <andre.goncalves@outsystems.com>
2019-07-15 00:37:34 +0100

    created a cocoa touch framework to replace the pod usage(because OS does not support pods using swift)

--- src/ios/ImageEditorPlugin.swift
+++ src/ios/ImageEditorPlugin.swift
@@ -1,5 +1,5 @@
 import UIKit
-import iOSPhotoEditor
+import OSPhotoEditor
 
 @objc(ImageEditorPlugin) class ImageEditorPlugin : CDVPlugin {
     
---
+++ src/ios/libs/OSPhotoEditor/OSPhotoEditor/TestOla.swift
@@ -0,0 +1,17 @@
+//
+//  TestOla.swift
+//  OSPhotoEditor
+//
+//  Created by André Gonçalves on 12/07/2019.
+//  Copyright © 2019 André Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+open class TestOla: NSObject {
+    
+    open func hello(){
+        debugPrint("Hello from AlamoWater!")
+        
+    }
+}
