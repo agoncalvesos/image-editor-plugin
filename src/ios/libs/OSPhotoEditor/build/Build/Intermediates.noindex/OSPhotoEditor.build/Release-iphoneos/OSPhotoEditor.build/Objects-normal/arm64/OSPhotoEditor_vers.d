2b8dddd551cd9a1fac5a6fc651730fbe27b62d70
André Gonçalves <andre.goncalves@outsystems.com>
2019-07-15 01:59:06 +0100

    no message

--- src/ios/libs/OSPhotoEditor/build/Build/Intermediates.noindex/OSPhotoEditor.build/Release-iphoneos/OSPhotoEditor.build/Objects-normal/arm64/OSPhotoEditor_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/veyron/Work/Outsystems/Plugins/ImageEditor/cordova/image-editor-plugin/src/ios/libs/OSPhotoEditor/build/Build/Intermediates.noindex/OSPhotoEditor.build/Release-iphoneos/OSPhotoEditor.build/DerivedSources/OSPhotoEditor_vers.c
