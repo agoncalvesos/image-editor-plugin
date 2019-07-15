96789feafee87971a3baece15fa1351fb1585c08
André Gonçalves <andre.goncalves@outsystems.com>
2019-07-15 01:45:08 +0100

    changed the framework to support devices and simulators (arm64, armv7, armv7s, x86_64 -arch i386)

---
+++ src/ios/libs/OSPhotoEditor/build/Build/Intermediates.noindex/OSPhotoEditor.build/Release-iphonesimulator/OSPhotoEditor.build/DerivedSources/OSPhotoEditor_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char OSPhotoEditorVersionString[];
+ extern const double OSPhotoEditorVersionNumber;
+
+ const unsigned char OSPhotoEditorVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OSPhotoEditor  PROJECT:OSPhotoEditor-1" "\n";
+ const double OSPhotoEditorVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ src/ios/libs/OSPhotoEditor/build/Build/Intermediates.noindex/OSPhotoEditor.build/Release-iphonesimulator/OSPhotoEditor.build/Objects-normal/i386/OSPhotoEditor_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/veyron/Work/Outsystems/Plugins/ImageEditor/cordova/image-editor-plugin/src/ios/libs/OSPhotoEditor/build/Build/Intermediates.noindex/OSPhotoEditor.build/Release-iphonesimulator/OSPhotoEditor.build/DerivedSources/OSPhotoEditor_vers.c
