eb87fa241a717a80db52e1184c01c35415617111
agoncalvesos <51172649+agoncalvesos@users.noreply.github.com>
2019-08-20 10:34:58 +0100

    Update ImageEditorPlugin.js

--- www/ImageEditorPlugin.js
+++ www/ImageEditorPlugin.js
@@ -1,9 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.editImageFromBase64 = function (base64, success, error) {
-    let reg = /base64,(.*)$/
-    let result = reg.exec(base64);
-    exec(success, error, 'ImageEditorPlugin', 'editImage', ["base64", result[1]]);
+    exec(success, error, 'ImageEditorPlugin', 'editImage', ["base64", base64]);
 };
 
 exports.editImageFromGallery = function (success, error) {
